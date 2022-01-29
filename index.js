a03f3389ffa75a7108201f37a22ac7a22b03338d
Gulzar Abdullabaeva <gulzar.abdullabaeva.kk@gmail.com>
2022-01-29 12:52:25 +0600

    third commit

--- index.js
+++ index.js
@@ -21,8 +21,7 @@
 // 5. Составить скрипт, который запрашивает у пользователя ввести номер кредитной карты. Скрипт должен обработать полученный номер и показать на странице последние 4 цифры а остальные цифры карты должны заменены на *.
     
 //     **Пример: "Номер вашей карты: **********1245"**
-let cardNumber = (+prompt("Урматтуу кардар карстаңыздын номерин жазыңыз!"));
-let cardLength = cardNumber.slice(0,cardNumber.length-4);
-let repStr = cardNumber.replace(cardLength,"************");
-let result = repStr.concat(cardNumber.slice(-12));
-console.log(result);
+let cardNumber = prompt("Урматтуу кардар карстаңыздын номерин жазыңыз!");
+let card = cardNumber.slice(12,16);
+console.log("********" + card);
+
