68cb706e02c05aab0dac7091a06e57f97d87cd84
Gulzar Abdullabaeva <gulzar.abdullabaeva.kk@gmail.com>
2022-01-30 15:57:57 +0600

    fourth commit

---
+++ script2.js
@@ -0,0 +1,4 @@
+// 2. Дана строка **'js'**. Сделайте из нее строку **'JS'**.
+let a = "js";
+let newA = a.toUpperCase(); 
+console.log(newA);
---
+++ script4.js
@@ -0,0 +1,4 @@
+// 4. Дана строка**'I am a hero!'**. Выведите в консоль **количество символов** в этой строке.
+let str = "I am a hero!";
+let newStr = str.length;
+console.log(newStr);
